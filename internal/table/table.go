2e3d95dd9de17d2b11b3b5bd82633369d526daf9
Simon LEONARD <git-1001af4@sinux.sh>
2024-01-12 15:33:42 +0100

    enhance: parallelize table creation

--- internal/table/table.go
+++ internal/table/table.go
@@ -52,21 +52,43 @@ type table struct {
 func NewTable(config config.Config, largeur uint64, hauteur uint64, random bool) (table, error) {
 	T := make([][]uint64, hauteur)
 
-	for i := range T {
-		// INIT
-		T[i] = make([]uint64, 2)
-		if random {
-			T[i][0] = index_aleatoire(&config)
-		} else {
-			T[i][0] = uint64(i)
-		}
+	// Initial code was sequential, I rewrote it to be concurrent.
+	// with size=2, abc=26, width=1000 and height=2000, I reduced the time from ~19s to ~5s
+	type result struct {
+		index uint64
+		err   error
+	}
 
-		// FILL
-		var err error
-		T[i][1], err = nouvelle_chaine(&config, T[i][0], largeur)
-		if err != nil {
-			return table{}, nil
+	results := make(chan result)
+
+	for i := range T {
+		go func(idx uint64) {
+			// INIT
+			T[idx] = make([]uint64, 2)
+			if random {
+				T[idx][0] = index_aleatoire(&config)
+			} else {
+				T[idx][0] = uint64(idx)
+			}
+
+			// FILL
+			var err error
+			T[idx][1], err = nouvelle_chaine(&config, T[idx][0], largeur)
+			results <- result{index: idx, err: err}
+		}(uint64(i))
+	}
+
+	var errors []error
+
+	for range T {
+		res := <-results
+		if res.err != nil {
+			errors = append(errors, res.err)
 		}
+	}
+
+	if len(errors) > 0 {
+		return table{}, errors[0]
 	}
 
 	sort.Slice(T, func(i, j int) bool {
